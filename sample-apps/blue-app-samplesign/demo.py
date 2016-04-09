041e02e91542d414fa15100b25057d800834efd3
BTChip <hello@ledger.fr>
2016-04-09 15:37:12 +0200

    Support a long message, display something on user cancellation

--- sample-apps/blue-app-samplesign/demo.py
+++ sample-apps/blue-app-samplesign/demo.py
@@ -16,6 +16,7 @@
 #*  limitations under the License.
 #********************************************************************************
 from ledgerblue.comm import getDongle
+from ledgerblue.commException import CommException
 from secp256k1 import PublicKey
 
 textToSign = ""
@@ -28,10 +29,27 @@ while True:
 dongle = getDongle(True)
 publicKey = dongle.exchange(bytes("8004000000".decode('hex')))
 print "publicKey " + str(publicKey).encode('hex')
-apdu = bytes("80028000".decode('hex')) + chr(len(textToSign)) + bytes(textToSign)
-signature = dongle.exchange(apdu)
-print "signature " + str(signature).encode('hex')
-publicKey = PublicKey(bytes(publicKey), raw=True)
-signature = publicKey.ecdsa_deserialize(bytes(signature))
-print "verified " + str(publicKey.ecdsa_verify(bytes(textToSign), signature))
+try:
+	offset = 0
+	while offset <> len(textToSign):
+		if (len(textToSign) - offset) > 255:
+			chunk = textToSign[offset : offset + 255] 
+		else:
+			chunk = textToSign[offset:]
+		if (offset + len(chunk)) == len(textToSign):
+			p1 = 0x80
+		else:
+			p1 = 0x00
+		apdu = bytes("8002".decode('hex')) + chr(p1) + chr(0x00) + chr(len(chunk)) + bytes(chunk)
+		signature = dongle.exchange(apdu)
+		offset += len(chunk)  	
+	print "signature " + str(signature).encode('hex')
+	publicKey = PublicKey(bytes(publicKey), raw=True)
+	signature = publicKey.ecdsa_deserialize(bytes(signature))
+	print "verified " + str(publicKey.ecdsa_verify(bytes(textToSign), signature))
+except CommException as comm:
+	if comm.sw == 0x6985:
+		print "Aborted by user"
+	else:
+		print "Invalid status " + comm.sw 
 
